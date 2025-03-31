1ff41cad90afb9068b2c48718e7535ebbf5d03cf
PepVerger <pvergerc@alumnes.santjosepobrer.es>
2025-03-31 15:56:56 +0200

    Flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
